eb88685b383c93853329962bf29983eb792f5610
Shelley Wang <seashellsw@gmail.com>
2024-05-18 14:23:37 -0400

    just added loading and setup chunks

--- vignettes/char-part-test.R
+++ vignettes/char-part-test.R
@@ -1,2 +1,16 @@
 install.packages("EvoPhylo")
 
+# Setup & Output Appearance Chunks: 
+```{r, setup, echo=FALSE}
+knitr::opts_chunk$set(echo = TRUE, warning = FALSE, collapse = TRUE, dpi=300)
+```
+# Loading the EvoPhylo package
+```{r, eval = FALSE}
+library(EvoPhylo)
+```
+
+```{r, include=FALSE}
+devtools::load_all(".")
+```
+
+# 1. Generate distance matrix
